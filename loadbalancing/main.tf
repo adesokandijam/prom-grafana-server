f62b8c5910623c28ff268bfd2e92fc69a0ad6928
Abdulmajid Adesokan <abdulmajidadesokan@Abdulmajids-MacBook-Pro.local>
2022-09-10 18:37:01 +0100

    check ip

--- loadbalancing/main.tf
+++ loadbalancing/main.tf
@@ -9,6 +9,7 @@ resource "aws_lb_target_group" "grafana_tg" {
   port     = 3000
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  target_type = "ip"
   lifecycle {
     create_before_destroy = true
     ignore_changes        = [name]
