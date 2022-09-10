74ccc2de44b6a6ede8936d8f180e492ba43f9efc
Abdulmajid Adesokan <abdulmajidadesokan@Abdulmajids-MacBook-Pro.local>
2022-09-10 19:45:25 +0100

    route53

--- route53/main.tf
+++ route53/main.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "metrics" {
   zone_id = "Z07962563NKNUV1C86GSU"
   name    = "metrics.valwitter.com"
-  type    = "A"
+  type    = "CNAME"
   ttl     = 300
   records = [var.lb_dns]
 }
