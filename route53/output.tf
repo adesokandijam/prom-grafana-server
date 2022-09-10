83fae2b03f542c384cc4b515d9fd4b9c4f4aec28
Abdulmajid Adesokan <abdulmajidadesokan@Abdulmajids-MacBook-Pro.local>
2022-09-10 20:28:16 +0100

    check

--- route53/output.tf
+++ route53/output.tf
@@ -1,3 +1,3 @@
-output "cert_arn" {
-  value = aws_acm_certificate_validation.cert_validation.certificate_arn
-}
+# output "cert_arn" {
+#   value = aws_acm_certificate_validation.cert_validation.certificate_arn
+# }
